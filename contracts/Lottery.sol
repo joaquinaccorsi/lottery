5357b0c3520f8756c91008092b0bdcf8294ab618
Joaquín Accorsi <joaquinaccorsi@gmail.com>
2021-12-06 13:52:06 -0300

    Test

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,34 +1,41 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.9;
 
 contract Lottery {
     address public manager;
     address[] public players;
-    
-    function Lottery() public {
+
+    constructor () public {
         manager = msg.sender;
+
     }
-    
+
     function enter() public payable {
+
         require(msg.value > .01 ether);
+
+
         players.push(msg.sender);
+
     }
-    
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+
+    function random() public view returns (uint) {
+
+       return uint (keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
     }
-    
+
     function pickWinner() public restricted {
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
+        
+        uint index = random () % players.length;
+        payable(players[index]).transfer(address(this).balance);
         players = new address[](0);
     }
-    
-    modifier restricted() {
+
+    modifier restricted(){
         require(msg.sender == manager);
         _;
     }
-    
-    function getPlayers() public view returns (address[]) {
+
+    function getPlayers() public view returns (address[] memory){
         return players;
     }
-}   
+}
--- deploy.js
+++ deploy.js
@@ -3,9 +3,9 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
+  'fit carry helmet web airport error vendor method frozen scatter next long',
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
+  'https://rinkeby.infura.io/v3/e08d4be31a4a462c9031e0b8d09b9f06'
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
